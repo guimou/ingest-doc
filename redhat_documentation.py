dd6f44df7980b01de01f2a38153cc13b514f072a
Guillaume Moutier <guillaume.moutier@gmail.com>
2024-04-23 10:11:36 -0400

    Red Hat Documentation splitter

--- redhat_documentation.py
+++ redhat_documentation.py
@@ -1,10 +1,8 @@
 from typing import List
 
-from langchain_core.documents import Document
-
-from langchain_community.document_loaders.web_base import WebBaseLoader
 from bs4 import BeautifulSoup
-
+from langchain_community.document_loaders.web_base import WebBaseLoader
+from langchain_core.documents import Document
 
 
 class RedHatDocumentationLoader(WebBaseLoader):
@@ -13,67 +11,73 @@ class RedHatDocumentationLoader(WebBaseLoader):
     def load(self) -> List[Document]:
         """Load webpages as Documents."""
         soup = self.scrape()
-        title = soup.select_one("h1", {'class':'title'}).text
-        soup = soup.select_one(".book")
+
+        title = soup.select_one("h1", {'class':'title'}).text # Get title
+        soup = soup.select_one(".book") # Get main content
+
         # Remove unwanted sections
-        unwanted_classes = ["producttitle", "subtitle", "abstract", "legalnotice"]
+        unwanted_classes = ["producttitle", "subtitle", "abstract", "legalnotice", "calloutlist", "callout"]
         for unwanted_class in unwanted_classes:
             for div in soup.find_all("div", {'class':unwanted_class}): 
                 div.decompose()
+            for span in soup.find_all("span", {'class':unwanted_class}):
+                span.decompose()
             for header in soup.find_all("h2", {'class':unwanted_class}):
                 header.decompose()
         for hr in soup.find_all('hr'):
             hr.decompose()
+
         # Find and delete anchor tag with content "Legal Notice"
         for anchor in soup.find_all('a'):
             if anchor.text == "Legal Notice":
                 anchor.decompose()
+        
         # Unwrap unwanted tags
-        unwrap_tags = ["a", "div", "span", "strong", "section"]
+        unwrap_tags = ["div", "span", "strong", "section"]
         for tag in unwrap_tags:
             for match in soup.findAll(tag):
                 match.unwrap()
-        # Extract text from paragraphs
-        for match in soup.findAll('p'):
-            match.replace_with(match.text)
-        # Remove unwanted attributes
+        
+        # Transform description titles
+        for dt in soup.find_all('dt'):
+            if (dt.string):
+                dt.string.replace_with(f"-> {dt.string}")
+        
+        # Transform code blocks
+        for code in soup.find_all('pre', {'class':'programlisting'}):
+            try:
+                content = code.text
+                code.clear()
+                if 'language-yaml' in code['class']:
+                    code.string = f"```yaml\n{content}\n```"
+                elif 'language-json' in code['class']:
+                    code.string = f"```json\n{content}\n```"
+                elif 'language-bash' in code['class']:
+                    code.string = f"```bash\n{content}\n```"
+                elif 'language-python' in code['class']:
+                    code.string = f"```python\n{content}\n```"
+                elif 'language-none' in code['class']:
+                    code.string = f"```\n{content}\n```"
+                else:
+                    code.string = f"```\n{content}\n```"
+            except Exception as e:
+                print(f"Error processing code block: {e}")
+        for code in soup.find_all('pre', {'class':'screen'}):
+            try:
+                content = code.text
+                code.clear()
+                code.string = f"```console\n{content}\n```"
+            except Exception as e:
+                print(f"Error processing code block: {e}")
+        
+        # Remove all attributes
         for tag in soup():
-            for attribute in ["class", "id", "style"]:
-                del tag[attribute]
-        # Transform unordered lists into paragraphs
-        for ul in soup.find_all('ul'):
-            new_content = ""
-            for li in ul.find_all('li'):
-                content = li.text.replace('\n', '')
-                new_content += f" - {content}\n"
-            ul.replace_with(new_content)
-        # Transform ordered lists into paragraphs
-        for ol in soup.find_all('ol'):
-            new_content = ""
-            for idx, li in enumerate(ol.find_all('li'), start=1):
-                content = li.text.replace('\n', '')
-                new_content += f" {idx}. {content}\n"
-            ol.replace_with(new_content)
-        # Transform description lists into paragraphs
-        for dl in soup.find_all('dl'):
-            new_content = ""
-            for dt in dl.find_all('dt'):
-                new_content = dt.text.replace('\n', '')
-                temp_soup = BeautifulSoup('', 'html.parser')
-                new_tag = temp_soup.new_tag("h4")
-                new_tag.string = new_content
-                dt.replace_with(new_tag)
-            for dd in dl.find_all('dd'):
-                new_content = dd.text.replace('\n', '')
-                content = dd.replace_with(new_content)
-        # Unwrap dl
-        for dl in soup.findAll('dl'):
-            dl.unwrap()
-        # Finally, unwrap the body
-        #soup.unwrap()
-
-        text = str(soup)
-        text = text.replace("\t", "").replace("\xa0", " ")
-        print(f"title: {title}")
+            tag.attrs.clear()
+        
+        text = str(soup) # Convert to string
+        text = text.replace("\xa0", " ") # Replace non-breaking space
+        
+        # Add metadata
         metadata = {"source": self.web_path, "title": title}
+        
         return [Document(page_content=text, metadata=metadata)]
