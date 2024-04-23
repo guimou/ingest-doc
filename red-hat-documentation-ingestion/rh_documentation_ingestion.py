d6f238a7fe24211d4e23e8d9a826ec9688a83f6b
Guillaume Moutier <guillaume.moutier@gmail.com>
2024-04-23 17:50:42 -0400

    wip

---
+++ red-hat-documentation-ingestion/rh_documentation_ingestion.py
@@ -0,0 +1,48 @@
+import os
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Milvus
+import rh_documentation_processing as rhdp
+
+
+os.environ["TRANSFORMERS_VERBOSITY"] = "error"
+os.environ["TRANSFORMERS_NO_ADVISORY_WARNINGS"] = "1"
+
+
+def ingest_documentation(product_info, milvus):
+
+    MILVUS_HOST = milvus["MILVUS_HOST"]
+    MILVUS_PORT = milvus["MILVUS_PORT"]
+    MILVUS_USERNAME = milvus["MILVUS_USERNAME"]
+    MILVUS_PASSWORD = milvus["MILVUS_PASSWORD"]
+    MILVUS_COLLECTION = f"{product_info.product.replace('-', '_')}_{product_info.language.replace('-', '_')}_{product_info.version.replace('.', '_')}"
+
+    splits = rhdp.generate_splits(product_info.product,
+                                  product_info.product_full_name,
+                                  product_info.version,
+                                  product_info.language)
+
+    model_kwargs = {'trust_remote_code': True, 'device': 'cuda'}
+    embeddings = HuggingFaceEmbeddings(
+        model_name="nomic-ai/nomic-embed-text-v1",
+        model_kwargs=model_kwargs,
+        show_progress=True
+    )
+
+    db = Milvus(
+        embedding_function=embeddings,
+        connection_args={"host": MILVUS_HOST,
+                         "port": MILVUS_PORT,
+                         "user": MILVUS_USERNAME,
+                         "password": MILVUS_PASSWORD},
+        collection_name=MILVUS_COLLECTION,
+        metadata_field="metadata",
+        text_field="page_content",
+        auto_id=True,
+        drop_old=True
+        )
+
+    print(f"Uploading documents to collection {MILVUS_COLLECTION}")
+
+    db.add_documents(splits)
+
+    print("Finished!")
